a6cec4895bd44e10ca561e6c9e30200e5740d431
swarzzy <tsu.jczu@yandex.ru>
2021-04-23 23:51:15 +0700

    Add project files

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set ObjOutDir=build\obj\
+set BinOutDir=build\
+set OutName=scan
+
+set LibClangIncludeDir=ext/libclang/include
+set LibClangLibraries=ext/libclang/lib/clang_static_bundled.lib
+
+IF NOT EXIST %BinOutDir% mkdir %BinOutDir%
+IF NOT EXIST %ObjOutDir% mkdir %ObjOutDir%
+
+ctime -begin ctime.ctm
+
+set CommonDefines=/D_CRT_SECURE_NO_WARNINGS /D_CINDEX_LIB_
+set CommonCompilerFlags=/Gm- /fp:fast /GR- /EHsc /nologo /diagnostics:classic /WX /std:c++17 /Zi /Od /GL /MT /Fd%BinOutDir% /I%LibClangIncludeDir%
+set CommonLinkerFlags=/INCREMENTAL:NO /OPT:REF /MACHINE:X64 /OUT:%BinOutDir%\%OutName%.exe /PDB:%BinOutDir%\%OutName%.pdb %LibClangLibraries% version.lib
+
+cl /MP /W3 /Fo%ObjOutDir% %CommonDefines% /I%ClReflectIncludeDirectory% %CommonCompilerFlags% src/Main.cpp /link %CommonLinkerFlags%
+
+rmdir /S /Q %ObjOutDir%
+del build\%OutName%.exp >NUL 2>&1
+del build\%OutName%.lib >NUL 2>&1
+
+ctime -end ctime.ctm
